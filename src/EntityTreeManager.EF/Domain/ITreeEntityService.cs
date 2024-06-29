5bb0e490419ae1b2765f99f9fbf5323b6fa012e8
denissfloww <denbugackoff21@gmail.com>
2024-06-29 14:30:25 +0500

    Added basic classes

---
+++ src/EntityTreeManager.EF/Domain/ITreeEntityService.cs
@@ -0,0 +1,17 @@
+﻿namespace EntityTreeManager.EF.Domain;
+
+public interface ITreeEntityService<TEntity, TId> where TEntity : TreeEntity<TId>
+    where TId : struct
+{
+    IQueryable<TreeEntity<TId>> GetRoots();
+
+    IQueryable<TreeEntity<TId>> GetChildren(TId id);
+
+    IQueryable<TreeEntity<TId>> GetChildren(TreeEntity<TId> category);
+
+    Task<TreeEntity<TId>?> GetByIdAsync(TId id);
+
+    Task<TreeEntity<TId>?> GetParentAsync(TreeEntity<TId> category);
+
+    Task<TreeEntity<TId>?> GetParentAsync(TId categoryId);
+}
---
+++ src/EntityTreeManager.EF/Domain/TreeEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace EntityTreeManager.EF.Domain;
+
+public abstract class TreeEntity<TId> where TId : struct
+{
+    public TId Id { get; set; }
+
+    public TId? ParentId { get; set; }
+    public TreeEntity<TId>? Parent { get; set; }
+
+    public IList<TreeEntity<TId>>? Children { get; set; }
+
+    public bool HasChildren => Children is { Count: > 0 };
+}
