4dd74e6c371851d23bb959d004027b56ffc72a4f
denissfloww <denbugackoff21@gmail.com>
2024-07-23 21:36:45 +0500

    Implement entity framework model builder extension

---
+++ src/EntityTreeManager.EF/Infrastructure/ModelBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using EntityTreeManager.EF.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityTreeManager.EF.Infrastructure;
+
+public static class ModelBuilderExtensions
+{
+    public static ModelBuilder UseTreeEntityConfiguration<TEntity, TId>(this ModelBuilder modelBuilder)
+        where TEntity : TreeEntity<TId>
+        where TId : struct
+    {
+        modelBuilder.ApplyConfiguration(new TreeEntityConfiguration<TEntity, TId>());
+        return modelBuilder;
+    }
+}
